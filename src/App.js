0a58bf63ada8d87b75809c731fd50ef48ec57f69
Jeremy Jones <jeremy@speakunique.co.uk>
2021-11-22 17:37:04 +0000

    evo

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import 'bulma/css/bulma.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AudioList } from './screens/AudioList';
 
-const App = () => {
-  return (
-    <Router basename={'/xadmin'}>
-      <Routes>
-        <Route path="/audio/" element={AudioList()} />
-      </Routes>
-    </Router>
-  );
-};
-
-export default App;
-
-// export default function App() {
+// const App = () => {
 //   return (
-//     <div>
-//       <h1>Project Still webclient backend</h1>
-//       <p>Start editing to see some magic happen :)</p>
-//     </div>
+//     <Router basename={'/xadmin'}>
+//       <Routes>
+//         <Route path="/audio/" element={AudioList()} />
+//       </Routes>
+//     </Router>
 //   );
-// }
+// };
+
+// export default App;
+
+export default function App() {
+  return (
+    <div>
+      <h1>Project Still webclient backend</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
